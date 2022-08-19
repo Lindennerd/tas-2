262291b97f8812b230d1a16213401e41172180d6
lindennerd <lindennerd@gmail.com>
2022-08-19 06:59:29 -0300

    fixing sections infra

--- src/pages/sections/index.tsx
+++ src/pages/sections/index.tsx
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import { SectionList } from "@/components/Sections/SectionList";
-import { Filter, FilterForm } from "@/components/UI";
-import { trpc } from "@/utils/trpc";
-import { NextPage } from "next";
-import { useState } from "react";
-
-const SectionsPage: NextPage = () => {
-  const [enableQuery, setEnableQuery] = useState(false);
-
-  function filter(data: FilterForm) {}
-
-  return (
-    <div>
-      <Filter onSubmit={filter} />
-      <SectionList />
-    </div>
-  );
-};
-
-export default SectionsPage;
-=======
 export default function SectionsPage() {
   return <div>Seções</div>;
 }
->>>>>>> acea6c11065b12344c094d305aeeaaf00dbfc347
--- src/schemas/section.schema.ts
+++ src/schemas/section.schema.ts
@@ -7,3 +7,14 @@ export const sectionSchema = z.object({
   description: z.string(),
   questions: z.array(questionSchema),
 });
+
+export const sectionInputSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+});
+
+export const sectionEditSchema = sectionInputSchema.extend({
+  id: z.string(),
+});
+
+export type Section = z.infer<typeof sectionSchema>;
